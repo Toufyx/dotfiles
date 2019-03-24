59bce0f3e1be30b7ef4acc5e409c5cd56121861e
Thibault DEFEYTER <defeyter.thibault@gmail.com>
2019-03-24 23:44:28 +0100

    add custom fish prompt in fish installation

--- tools/fish.debian.sh
+++ tools/fish.debian.sh
@@ -1,3 +1,11 @@
 # /bin/bash
 
+# install package
 apt-get install fish
+
+# add custom fish prompt
+mkdir -p ~/.config/fish/functions/
+wget https://raw.githubusercontent.com/Toufyx/environment/master/fish/functions/fish_prompt.remote.fish -O ~/.config/fish/functions/fish_prompt.fish
+
+# update the default shell to fish
+chsh -s `which fish`
