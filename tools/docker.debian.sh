c1c0dd987015311fd0afc605fb7425d444bd3f00
Thibault DEFEYTER <defeyter.thibault@gmail.com>
2019-03-24 22:53:22 +0100

    add my favorite editor and esssential development tools

---
+++ tools/docker.debian.sh
@@ -0,0 +1,18 @@
+# /bin/bash
+
+apt-get update
+
+# Install packages to allow apt to use a repository over HTTPS:
+apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+# Add Docker’s official GPG key:
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+# add the docker repository
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+# Update the apt package index.
+apt-get update
+
+# Install the latest version of Docker CE and containerd, or go to the next step to install a specific version:
+apt-get install docker-ce docker-ce-cli containerd.io
