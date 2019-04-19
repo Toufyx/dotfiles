c30e5a0f4896f0d24a713a70a9f086e9a6e778be
Thibault DEFEYTER <defeyter.thibault@gmail.com>
2019-04-19 11:10:16 +0200

    add git aliases

---
+++ tools/git.aliases.sh
@@ -0,0 +1,10 @@
+# /bin/sh
+#
+# Set up common Git Aliases
+# See https://git-scm.com/book/en/v2/Git-Basics-Git-Aliases for more information
+#
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
