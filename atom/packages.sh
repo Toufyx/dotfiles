2b97318c39d0d27b2891f5565ac1ca7cbad7a14b
Thibault DEFEYTER <defeyter.thibault@gmail.com>
2019-04-19 11:50:15 +0200

    add atom packages configuration

---
+++ atom/packages.sh
@@ -0,0 +1,21 @@
+# /bin/sh
+#
+# Install Atom Packages
+#
+
+# Themes and Color Synthaxes
+apm install atom-material-ui
+apm install predawn-syntax
+
+# General Useful Tools
+apm install git-blame
+apm install goto-definition
+apm install linter
+apm install pretty-json
+apm install sort-lines
+
+# Python Dev environment
+apm install autocomplete-python
+apm install linter-flake8
+apm install python-black
+apm install python-isort
