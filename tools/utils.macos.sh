f0a0d7db7c80bc26d23944cdc4cf6fb475955ea3
DEFEYTER Thibault <defeyter.thibault@gmail.com>
2019-04-23 09:50:03 +0200

    add various utilitary tools for mac os

---
+++ tools/utils.macos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Install Various Tools on MacOS System
+# Required HomeBrew Installation
+#
+
+# WGET is not part of the mac os standard lib ... and yet so powerful !
+brew install wget
+
+# JQ is a command line JSON parser that allows to make data query over JSON structures
+brew install jq
