dcb76353eaf1d9a7944fbcc0eda003adf3aabf84
DEFEYTER Thibault <defeyter.thibault@gmail.com>
2019-09-11 12:57:53 +0200

    fix vscode installation file

--- vscode/install.macos.sh
+++ vscode/install.macos.sh
@@ -4,7 +4,7 @@
 # with command line tool already configured
 
 # deploy the settings
-cp config/settings.json "~/Library/Application Support/Code/User/settings.json"
+cp config/settings.json ~/Library/Application\ Support/Code/User
 
 # install listed extensions
 for extension in $(cat config/code.extensions); do code --install-extension $extension; done
