8ce8a2a8522a68c8d0368e8f047b1172687c6405
DEFEYTER Thibault <defeyter.thibault@gmail.com>
2019-09-11 12:00:19 +0200

    improve alacritty set up

--- alacritty/install.macos.sh
+++ alacritty/install.macos.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# install alacritty
-brew cask install alacritty
+# install elacritty if not already there
+brew cask list alacritty || brew cask install alacritty
+
+# using alacritty without a terminal multiplexer is kinda impossible...
+brew list tmux || brew install tmux
 
 # deploy alacritty configuration
+mkdir -p ~/.config/alacritty/
 cp config/alacritty.yml ~/.config/alacritty/alacritty.yml
