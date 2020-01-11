106c8c393bf7f93d07cd9142ad7765138016bea4
Thibault DEFEYTER <defeyter.thibault@gmail.com>
2020-01-11 17:25:13 +0100

    clean and improve alacritty installer

--- alacritty/install.macos.sh
+++ alacritty/install.macos.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+SCRIPT_DIR="$(dirname "$0")"
+
 # install elacritty if not already there
 brew cask list alacritty || brew cask install alacritty
 
 # deploy alacritty configuration
 mkdir -p ~/.config/alacritty/
-cp config/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp $SCRIPT_DIR/config/alacritty.yml ~/.config/alacritty/alacritty.yml
