eb4c24f105daea512da7bb87970110b33756e92b
Thibault DEFEYTER <defeyter.thibault@gmail.com>
2020-01-11 17:31:56 +0100

    improve and clean vscode config installer

--- vscode/install.macos.sh
+++ vscode/install.macos.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-
 # We assume an instance of VSCode is up and running
 # with command line tool already configured
 
+SCRIPT_DIR="$(dirname "$0")"
+
 # deploy the settings
-cp config/settings.json ~/Library/Application\ Support/Code/User
+cp $SCRIPT_DIR/config/settings.json ~/Library/Application\ Support/Code/User
 
 # install listed extensions
-for extension in $(cat config/code.extensions); do code --install-extension $extension; done
+for extension in $(cat $SCRIPT_DIR/config/code.extensions); do code --install-extension $extension; done
