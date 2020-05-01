8a81cd712c0d273b5325d45ce1c3058b0a969d87
Thibault DEFEYTER <defeyter.thibault@gmail.com>
2020-05-01 12:19:55 +0200

    improve zsh installation and zshrc

--- zsh/install.macos.sh
+++ zsh/install.macos.sh
@@ -2,11 +2,16 @@
 # ZSH is install by default on mac os systems,
 # Nothing to do here except install package manager and config files
 
-SCRIPT_DIR="$(dirname "$0")"
+SCRIPT_DIR=`realpath "$(dirname "$0")"`
+WORKSPACE_DIR="$HOME/Workspace/bin"
 
 # install antigen package manager
-mkdir -p ~/Workspace/bin
-curl -L git.io/antigen > ~/Workspace/bin/antigen.zsh
+mkdir -p $WORKSPACE_DIR
+if [[ ! -f $WORKSPACE_DIR/antigen.zsh ]]
+then
+    curl -L git.io/antigen > $WORKSPACE_DIR/antigen.zsh
+fi
 
 # deploy zshrc file
-cp $SCRIPT_DIR/config/zshrc.macos.user ~/.zshrc
+rm ~/.zshrc
+ln -s $SCRIPT_DIR/config/zshrc.macos.user $HOME/.zshrc
