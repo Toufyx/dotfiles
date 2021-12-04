51d310ca23347dcee54cdd45344edbee623ffb5f
Thibault DEFEYTER <defeyter.thibault@gmail.com>
2021-12-04 14:58:05 +0100

    [tmux] add tmux session config

--- tmux/install.macos.sh
+++ tmux/install.macos.sh
@@ -5,6 +5,9 @@ SCRIPT_DIR=`realpath "$(dirname "$0")"`
 # install elacritty if not already there
 brew install tmux
 
-# deploy tmux configuration file
+# deploy tmux configurations file
+mkdir -p $HOME/.tmux/
 rm $HOME/.tmux.conf
+rm $HOME/.tmux/session.conf
 ln -s $SCRIPT_DIR/config/tmux.conf $HOME/.tmux.conf
+ln -s $SCRIPT_DIR/config/session.conf $HOME/.tmux/session.conf
