96cc4ec0f4c672c1ce7a7a6f63d05a294aeb07e6
Thibault DEFEYTER <defeyter.thibault@gmail.com>
2022-02-12 21:54:30 +0100

    [nvim] remove airline

--- neovim/config/lua/plugins.lua
+++ neovim/config/lua/plugins.lua
@@ -7,9 +7,6 @@ Plug('nvim-lua/plenary.nvim')
 -- gruvbox colors scheme
 Plug('morhetz/gruvbox')
 
--- the amazing vim lualine with icons
-Plug('vim-airline/vim-airline')
-
 -- generic abstract syntax tree manager, mainly used for awesome color schemes
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
 Plug('nvim-treesitter/nvim-treesitter-textobjects')
