f6bb9cd92e462c8af2c56edb9711d55341bd0924
Thibault Defeyter <defeyter.thibault@gmail.com>
2022-08-18 15:27:15 +0200

    [neovim] add neovim plugin manager

--- neovim/install.macos.sh
+++ neovim/install.macos.sh
@@ -17,3 +17,7 @@ brew install neovim
 mkdir -p $HOME/.config/
 rm -rf $HOME/.config/nvim
 ln -s $SCRIPT_DIR/config $HOME/.config/nvim
+
+# install neovim plugin manager
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
